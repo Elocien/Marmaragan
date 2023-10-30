f61260ed9109e5fd5280e9f86737aa88624f01ea
Elocien <lucianmcintyre@gmail.com>
2023-10-30 14:58:58 +0100

    Distributed implementation to seperate files

---
+++ main.py
@@ -0,0 +1,72 @@
+from langchain.chat_models import ChatOpenAI
+from util.ada_to_json import ada_to_json
+from util.gnatprove import run_gnatprove
+from llm.langchain_impl import *
+
+import logging
+from datetime import datetime
+
+
+
+#Params
+# ---------------------------------------------------------------------------
+# Model
+llm_model = "gpt-3.5-turbo-0613" #or "gpt-4"
+llm_temperature = 0
+llm = ChatOpenAI(model_name=llm_model, temperature=llm_temperature)
+
+# ADA/SPARK2014 Project Location
+project_location = ""
+
+# Input File
+input_file = '/Users/lucian/Documents/Uni/Projects/Diplomarbeit/spark_by_example/binary-search/search_lower_bound_p.adb'
+
+# Prompt Text
+prompt_text = "Add appropriate pragma Loop_Invariant statements to the following ada/spark2014 code, so that gnatprove may be run without errors. Only return the code"
+# ---------------------------------------------------------------------------
+
+
+
+
+# Logging Setup
+# Set up the logger
+logging.basicConfig(filename='history.log', level=logging.INFO,
+                    format='%(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+
+
+
+
+# Convert given ada file into a prompt template (.json)
+ada_to_json(input_file, prompt_text)
+
+
+# Load the prompt
+prompt = get_prompt("prompt.json")
+
+# Get the LLM Response
+response, tokens = generate_llm_response(llm_model, llm_temperature, prompt)
+
+
+# Take the response and write to file
+
+
+# Compile the project using Alire 
+run_gnatprove(project_location)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Log
+logging.info(f" Model: {llm_model} | Temperature: {llm_temperature} | Tokens Consumed: {tokens} \nUser Message: {prompt_text} \nInput File: {input_file} \nLLM Response: \n{response}\nGnatProve Output: \n\n")
