f8d0e1850f44ea23fc5c06856e351da81e6331ee
Elocien <lucianmcintyre@gmail.com>
2023-12-11 17:37:40 +0100

    further impl

--- src/gen_n.py
+++ src/gen_n.py
@@ -1,5 +1,11 @@
+import subprocess
+from time import sleep
 from src.util import *
-from assistant_manager import openai_assistant
+from src.assistant_manager import openai_assistant
+from openai import OpenAI
+import shutil
+import asyncio
+
 
 
 class gen_1:
@@ -22,15 +28,55 @@ class gen_1:
     
     def run_benchmark(self):
         
+        benchmark_dir = "spark_benchmark"
+        
         # Create the assistant
-        assistant = openai_assistant(self.instructions)
+        # assistant = openai_assistant(self.instructions)
         
-        # For each file in the benchmark, run gnatprove and extract the medium and line of code
+    # For each file in the benchmark, run gnatprove and extract the medium and line of code
         
         
-        # For item in benchmark, get the associated medium and concatenate to the code example, then run assistant.create_message(item)
+        # Remove dir if it exists 
+        try:
+            shutil.rmtree(benchmark_dir)
+            sleep(1)
+        except Exception:
+            pass
         
-        # assistant.retrieve_messages()
+        # generate temporary directory for the spark files
+        os.mkdir(benchmark_dir)
+        
+        benchmark_gpr_files = []
+        
+        # Iterate over each file in the benchmark and generate the files in the spark_benchmark directory
+        for file_path in self.benchmark_files:
+            gpr_file_path = generate_spark_files(file_path, benchmark_dir)
+            benchmark_gpr_files.append(gpr_file_path)
+        
+    # For item in benchmark, get the associated medium and concatenate to the code example, then run assistant.create_message(item)
+        
+        for project in benchmark_gpr_files:
+            # Run gnatprove on the project
+            mediums = asyncio.run(run_gnatprove(project))
+            
+            
+            # Iterate over each medium and extract line of code and error message
+            
+
+        
+    # Retrieve the messages from the assistant
+        # messages = assistant.retrieve_messages()
+        
+    # For each message, extract the fixed code and write to file in the benchmark_dir
+    
+    # Run gnatprove on the fixed code and extract any mediums 
+    
+    # Delete the assistant
+        # assistant.delete_assistant()
+        
+    # Delete the temporary directory
+        # shutil.rmtree(benchmark_dir)
+
     
     
     
