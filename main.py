8bc71f2d260adefdf9974d84c5bb44319cc5a2b1
Elocien <lucianmcintyre@gmail.com>
2023-12-15 15:34:43 +0100

    Typo

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.gen_n import *
 
 basic_benchmark = gen_1(instructions=
                         """\
-                        You are a Spark2014/ADA programmer. Given code in the form of an implementation (abd), specification (ads)
+                        You are a Spark2014/ADA programmer. Given code in the form of an implementation (adb), specification (ads)
                         and additional dependencies , fix the code so that it runs error free. The compile errors (mediums) from gnatprove
                         are additionally given. Fix the code by adding Pragma Loop_Invariant statements.
                         Return only the fixed code for the first implemenation file.\
