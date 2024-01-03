cae7eb0474edb8f573ee13317551886b645ca648
Elocien <lucianmcintyre@gmail.com>
2024-01-03 13:08:24 +0100

    assistant instruction changes

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ basic_benchmark = gen_1(instructions=
                         """\
                         You are a Spark2014/ADA programmer. Given code in the form of an implementation (adb), specification (ads)
                         and additional dependencies , fix the code so that it runs error free. The compile errors (mediums) from gnatprove
-                        are additionally given. Fix the code by adding Pragma Loop_Invariant statements.
+                        are additionally given. Fix the code so that no mediums occur.
                         Return only the fixed code for the first implemenation file.\
                         """, 
                         benchmark_dir="spark_projects/benchmark/code")
