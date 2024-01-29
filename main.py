c61970158d369d11cacd8ed65952270fa164528d
Elocien <lucianmcintyre@gmail.com>
2024-01-29 11:51:32 +0100

    Prompt changes

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ basic_benchmark = gen_1(instructions=
                         """\
                         You are a Spark2014/ADA programmer. Given code in the form of an implementation (adb), specification (ads)
                         and additional dependencies , fix the code so that it runs error free. The compile errors (mediums) from gnatprove
-                        are additionally given. Fix the code so that no mediums occur.
-                        Return only the fixed code for the first implemenation file.\
+                        are additionally given. Fix the code by adding a single pragma Loop_Invariant statement. 
+                        Return only the fixed code for a single file.\
                         """, 
-                        benchmark_dir="spark_projects/benchmark/code",
+                        benchmark_dir="spark_projects/benchmark_rem_first_LoopInv",
                         gpt_model = "gpt-3.5-turbo-1106")
 
 basic_benchmark.run_benchmark()
