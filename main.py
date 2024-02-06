d83b85b4772c06092d56b59c6d9209e7686825e3
Elocien <lucianmcintyre@gmail.com>
2024-02-06 14:29:12 +0100

    comments

--- main.py
+++ main.py
@@ -9,10 +9,12 @@ basic_benchmark = gen_1(instructions=
                         You are a Spark2014/ADA programmer. Given code in the form of an implementation (adb), specification (ads)
                         and additional dependencies , fix the code so that it runs error free. The compile errors (mediums) from gnatprove
                         are additionally given. Fix the code by adding a single pragma Loop_Invariant statement. 
-                        Return only the fixed code for a single file.\
+                        Return a single file, with the code appropriatly fixed.\
                         """, 
                         benchmark_dir="spark_projects/benchmark_rem_first_LoopInv",
-                        gpt_model = "gpt-3.5-turbo-1106")
+                        gpt_model="gpt-4-0613")
+
+# gpt_model (str): The GPT model to use. Choice of "gpt-3.5-turbo-1106", "gpt-4-0125-preview" or "gpt-4-0613"
 
 basic_benchmark.run_benchmark()
 
