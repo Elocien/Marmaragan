7dfdbf6f23721b14ff9c426752fd10a393aac34a
Elocien <lucianmcintyre@gmail.com>
2024-02-14 09:46:08 +1000

    cleaned main and adjusted instructions

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
 from src.gen_n import *
-from scripts.gen_benchmark import gen_benchmark
+
+
+# GPT Model to use
+
+model = "gpt-3.5-turbo-1106"
+# model = "gpt-4-0125-preview"
+# model = "gpt-4-0613"
+
 
 
 
 # Run the benchmark
 basic_benchmark = gen_1(instructions=
                         """\
-                        You are a Spark2014/ADA programmer. Given code in the form of an implementation (adb), specification (ads)
-                        and additional dependencies , fix the code so that it runs error free. The compile errors (mediums) from gnatprove
-                        are additionally given. Fix the code by adding a single pragma Loop_Invariant statement. 
-                        Return a single file, with the code appropriatly fixed.\
+                        You are a Spark2014/ADA programmer. You must complete the package body of the given program, inserting a single "pragma Loop_Invariant"
+                        statement directly within a "loop" statement. E.g. a statement such as "pragma Loop_Variant (Increases => L.Size);"
+                        Return only the code of the amended package body, with the single addition. Do not modify any other parts of the code!\
                         """, 
                         benchmark_dir="spark_projects/benchmark_rem_first_LoopInv",
-                        gpt_model="gpt-4-0613")
+                        gpt_model=model)
 
-# gpt_model (str): The GPT model to use. Choice of "gpt-3.5-turbo-1106", "gpt-4-0125-preview" or "gpt-4-0613"
+
 
 basic_benchmark.run_benchmark()
 
 
 
-# Generate the spark benchmark files
-# gen_benchmark(benchmark_dir="spark_projects/benchmark_rem_first_LoopInv")
 
 
