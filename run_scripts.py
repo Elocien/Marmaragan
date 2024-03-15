ffad1d350f7fdc29b546b8d88d1b2cd8e0c5ad48
Elocien <lucianmcintyre@gmail.com>
2024-03-15 14:44:23 +0100

    scripts

--- run_scripts.py
+++ run_scripts.py
@@ -4,12 +4,12 @@ from scripts.gen_benchmark import gen_benchmark
 
 
 # Generate the spark benchmark files
-gen_benchmark(benchmark_dir="benchmarks/benchmark_rem_first_LoopInv")
-# gen_benchmark(benchmark_dir="benchmarks/benchmark/code")
+# gen_benchmark(benchmark_dir="benchmarks/benchmark_rem_last_LoopInv")
+gen_benchmark(benchmark_dir="benchmarks/benchmark/code")
 
-# # Generate the diff between the original and modified benchmark
+# Generate the diff between the original and modified benchmark
 # original_dir = 'benchmarks/benchmark/code'
-# modified_dir = 'benchmarks/benchmark_rem_first_LoopInv'
+# modified_dir = 'benchmarks/benchmark_rem_last_LoopInv'
 # differences = calculate_diff_with_line_numbers(original_dir, modified_dir)
 
 # for diff in differences:
