6b5dfecd8a100cb5eede544dd4767aed9d17a5e1
Elocien <lucianmcintyre@gmail.com>
2024-03-29 11:06:24 +0100

    Refactor of code for modularity and clarity

--- main.py
+++ main.py
@@ -1,36 +1,122 @@
 from src.run_benchmark import *
 
 
-# GPT Model to use
+# PARAMS
 
+
+# System Message
+# -----------------------------------------------------------------------
+system_message="""\
+You are a Spark2014/ADA programmer with strong logical reasoning abilities. 
+You are tasked with fixing implemenations of Spark2014/ADA programs. You will be given an Implementation of a program, as
+well as a specification of the program. You must complete the package body of the given program, inserting a single "pragma Loop_Invariant" statement.
+You must not modify the code in any other way.
+"""
+
+
+# Prompt
+# -----------------------------------------------------------------------
+
+# Zero-shot-CoT prompt
+prompt = """\n
+Try to solve the following problem logically and step by step. The final answer should then be delimited in the following way:
+
+```ada
+
+code here
+
+```
+
+The following are the specifications and dependencies of a Spark2014/ADA project:
+
+{dependencies} \n
+
+This is the package body (implementation):
+
+{package_body} \n
+
+Add a single 'pragma Loop_Invariant' statement to the package body, so that the code runs error and medium free. 
+Do not modify the code in any other way. Return the entire implementation file with the single addition.
+"""
+
+
+# # Normal Prompt
+# prompt = f"""\
+# The following code is a Spark2014/ADA project.\n\n
+# {dependencies}
+
+
+# Here are the current mediums being thrown by gnatprove, in the form of the line referenced as the cause and an explaination:\n\n
+# {package_body}
+
+# Return the fixed code for the first implementation file, delimiting with 
+# ```ada
+
+# code here
+
+# ```
+# """
+
+                    
+
+# Benchmark
+# -----------------------------------------------------------------------
+benchmark_file_path = "benchmarks/benchmark_rem_last_LoopInv"
+# benchmark_file_path = "benchmarks/benchmark_rem_first_LoopInv"
+
+
+# GPT Model
+# -----------------------------------------------------------------------
 model = "gpt-3.5-turbo-1106"
 # model = "gpt-4-0125-preview"
 # model = "gpt-4-0613"
 
 
+# N solutions
+# -----------------------------------------------------------------------
+n = 1
+
+
+# Retries
+# -----------------------------------------------------------------------
+retries = 0
+
+
+# With mediums in prompt (pre-compile program with gnatprove)
+# -----------------------------------------------------------------------
+with_medium_in_prompt = False
+
+
+# Which benchmark programs to run (default is all if option is excluded)
+# -----------------------------------------------------------------------
+# All programs 
+# benchmark_programs = list(range(1, 17))
+
+# # Only argu
+# benchmark_programs = [2,3,4,5,6]
+
+# # Only non-argu
+# benchmark_programs = [1,7,8,9,10,11,12,13,14,15,16]
+
+# Only copy
+benchmark_programs = [7]        
+
 
 
 # Run the benchmark
-basic_benchmark = run_benchmark(system_message=
-                        # """\
-                        # You are a Spark2014/ADA programmer. You must complete the package body of the given program, inserting a single "pragma Loop_Invariant"
-                        # statement directly within a "loop" statement. E.g. a statement such as "pragma Loop_Variant (Increases => L.Size);"
-                        # Return only the code of the amended package body, with the single addition. Do not modify any other parts of the code!\
-                        # """, 
-                        """\
-                        You are a Spark2014/ADA programmer with strong logical reasoning abilities. 
-                        You are tasked with fixing implemenations of Spark2014/ADA programs. You will be given an Implementation of a program, as
-                        well as a specification of the program. You must complete the package body of the given program, inserting a single "pragma Loop_Invariant" statement.
-                        You must not modify the code in any other way.
-                        """, 
-                        benchmark_dir="benchmarks/benchmark_rem_last_LoopInv",
+basic_benchmark = run_benchmark(
+                        system_message=system_message,
+                        prompt=prompt,
+                        benchmark_dir=benchmark_file_path,
                         gpt_model=model,
-                        n_solutions=3,
-                        benchmark_program_indices=[10])
+                        n_solutions=n,
+                        retries=retries,
+                        with_medium_in_prompt=False,
+                        benchmark_program_indices=benchmark_programs)
 
 
 
-basic_benchmark.chain_of_thoughts()
+basic_benchmark.run()
 
 
 
--- src/run_benchmark.py
+++ src/run_benchmark.py
@@ -17,18 +17,21 @@ class run_benchmark:
     All results are logged to the benchmark_run.log file.
     
     Args:
-            system_message (str): Instructions detailing the purpose of the Assistant. Example: "You are a programmer. Fix the sent python code so that it runs correctly"
+            system_message (str): Instructions detailing the context for the language model
             benchmark_dir (str): The directory containing the benchmark files.
             gpt_model (str): The GPT model to use. Choice of "gpt-3.5-turbo-1106", "gpt-4-0125-preview" or "gpt-4-0613"
             benchmark_program_indices (List[int]): A list of the indices of the programs in the benchmark to run. Default is all programs in the benchmark
     """    
     
-    def __init__(self, system_message: str, benchmark_dir: str, gpt_model: str, n_solutions: int, benchmark_program_indices: List[int] = list(range(1, 17))):
+    def __init__(self, system_message: str, prompt: str, benchmark_dir: str, gpt_model: str, n_solutions: int, retries: int, with_medium_in_prompt: bool, benchmark_program_indices: List[int] = list(range(1, 17))):
         
         # Set the system message and gpt model
         self.system_message = system_message
+        self.prompt = prompt
         self.gpt_model = gpt_model
         self.n_solutions = n_solutions
+        self.retries = retries
+        self.with_medium_in_prompt = with_medium_in_prompt
         
         # Retrieve the benchmark files
         self.benchmark_txt_files = retrieve_benchmark_files(benchmark_dir, benchmark_program_indices)
@@ -64,7 +67,7 @@ class run_benchmark:
 
 
 
-    def init_run(self, prompt_type: str) -> List[Tuple[str, str]]:
+    def init_run(self) -> List[Tuple[str, str]]:
         """
         This class initialises the run and creates the temporary files for the benchmark.
         
@@ -97,14 +100,15 @@ Starting new Benchmark Run
 Model: {self.gpt_model} \n
 Programs: \n{nl.join(map(str, benchmark_programs))} \n
 n: {self.n_solutions}
-Prompt Type: {prompt_type} \n
+Retries: {self.retries}
+With Mediums in Prompt: {self.with_medium_in_prompt}
 System Message: \n{self.system_message}
+--------------------------
+Prompt: \n{self.prompt}\n
 --------------------------
 \n\n\n
                          """)
         
-        
-    # For each file in the benchmark, run gnatprove and extract the medium and line of code
         
         
         # Array of tuples, containing the results, of the form:
@@ -134,6 +138,15 @@ System Message: \n{self.system_message}
         
     
     def end_run(self, results_array: List[Tuple[bool, bool]]) -> None:
+        """
+        This class ends the run by sumarrising results in the log
+        
+        Args:
+            results_array (List[Tuple[bool, bool]]): A list of tuples containing the results of the form [(program-name, gnatprove-successful-compilation, medium-free)]
+        
+        Returns:
+            None
+        """
         
         successes = 0
         failures = 0
@@ -177,6 +190,12 @@ Summary of results:
     def invoke_llm(self, prompt: str) -> List[str]:
         """
         This class invokes the LLM to generate n solutions for a given prompt. If n is 1, it returns a single solution
+        
+        Args:
+            prompt (str): The prompt to send to the LLM
+            
+        Returns:
+            List[str]: A list of strings containing each of the responses
         """
         
         # List of solutions
@@ -192,10 +211,12 @@ Summary of results:
         # Create a human message
         message = HumanMessage(content=prompt)
         
-        response = chat_model._generate([message])  # Generate responses
+        # Generate responses
+        response = chat_model._generate([message])  
+        
         # Check if the response is a ChatResult object
         assert isinstance(response, ChatResult)
-        assert len(response.generations) == self.n_solutions  # Check if 5 responses are generated
+        assert len(response.generations) == self.n_solutions  # Check if correct number responses are generated
         for generation in response.generations:
             assert isinstance(generation.message, BaseMessage)
             assert isinstance(generation.message.content, str)
@@ -204,242 +225,75 @@ Summary of results:
         
         
         return solutions
+
+
+
     
     
-    def extract_code_and_write_file(self, llm_response: str, gpr_file_path: str, response_number_counter: int) -> bool:
+    # def gnatprove_pre_compile(self) -> None:
+    #     """
+    #     This class runs the benchmark, running gnatprove to identify any mediums and including this information in the prompt
+    #     """
+        
+    #     # Initialise the run and retrieve the benchmark files
+    #     benchmark_files = self.init_run("With Gnatprove mediums in Prompt")
+        
+    #     # f string newline fix   
+    #     nl = "\n"
+        
+        
+    # # Iterate over each project in the benchmark
+    #     for benchmark_file_path in benchmark_files:
+            
+    #         gpr_file_path = benchmark_file_path[0]
+            
+
+    #     # Run gnatprove on the project
+    #         output = run_gnatprove(gpr_file_path)
+    #         mediums = parse_gnatprove_output(output)
+            
+    #         project_dir = "/".join(gpr_file_path.split("/")[:-1])
+            
+
+    #         # Compile the lines of code, which are referenced in the mediums
+    #         # E.g. for medium: swap_ranges_p.adb:17:13: overflow check might fail, extract line 17 from file swap_ranges_p.adb
+    #         medium_code_reference = []
+            
+    #         for medium in mediums:
+    #             medium_code_reference.append(
+    #                 (extract_line_of_code_from_file(medium, project_dir), medium[1]))
+            
+            
+    #         # retrieve the benchmark txt file
+    #         benchmark_file = open(benchmark_file_path[1], "r")
+            
+    #         # Format medium code reference
+    #         medium_code_reference = "\n".join(f"Line: {line},\n Explanation: {explanation}\n" for line, explanation in medium_code_reference)
+            
+        
+
+
+
+
+
+
+    
+    
+    
+    
+    
+
+            
+
+
+    def run(self) -> None:
         """
-        Extracts the fixed code from the LLM response and writes it to a file in the benchmark directory.
-        Returns True if the extraction and writing is successful, False otherwise.
-        """
-        # Init compile success flag
-        compile_success = True
-
-        llm_code = ""
-        adb_file_path = ""
-
-        # Extract the code from the response
-        try:
-            llm_code = extract_code_from_response(llm_response)
-        except ValueError as e:
-            compile_success = False
-            self.logger.error(
-                f"\n-----------------------------------\nError extracting code from response number: {gpr_file_path.split('/')[-1]}:{response_number_counter}\nCode: {llm_response}\n-----------------------------------\n\n")
-
-        if llm_code not in ["", None]:
-
-            # Extract the filename from the response
-            try:
-                adb_filename = extract_filename_from_response(
-                    llm_code)
-
-                # Convert filename to lowercase and add .adb extension
-                filename_with_extension = adb_filename.lower() + ".adb"
-
-                project_dir = "/".join(gpr_file_path.split("/")[:-1])
-                adb_file_path = project_dir + "/" + filename_with_extension
-
-                # Overwrite the destination file with the response code
-                overwrite_destination_file_with_string(
-                    adb_file_path, llm_code)
-
-            except ValueError as e:
-                compile_success = False
-                self.logger.error(
-                    f"\n-----------------------------------\n\nError extracting filename from response: {gpr_file_path.split('/')[-1]}:{response_number_counter}\nCode: {llm_code}\n-----------------------------------\n\n")
-
-        return compile_success
-
-        
-
-
-
-    
-    
-    def gnatprove_pre_compile(self) -> None:
-        """
-        This class runs the benchmark, running gnatprove to identify any mediums and including this information in the prompt
-        """
-        
-        # Initialise the run and retrieve the benchmark files
-        benchmark_files = self.init_run("With Gnatprove mediums in Prompt")
-        
-        # f string newline fix   
-        nl = "\n"
-        
-        
-    # Iterate over each project in the benchmark
-        for benchmark_file_path in benchmark_files:
-            
-            gpr_file_path = benchmark_file_path[0]
-            
-
-        # Run gnatprove on the project
-            output = run_gnatprove(gpr_file_path)
-            mediums = parse_gnatprove_output(output)
-            
-            project_dir = "/".join(gpr_file_path.split("/")[:-1])
-            
-
-            # Compile the lines of code, which are referenced in the mediums
-            # E.g. for medium: swap_ranges_p.adb:17:13: overflow check might fail, extract line 17 from file swap_ranges_p.adb
-            medium_code_reference = []
-            
-            for medium in mediums:
-                medium_code_reference.append(
-                    (extract_line_of_code_from_file(medium, project_dir), medium[1]))
-            
-            
-            # retrieve the benchmark txt file
-            benchmark_file = open(benchmark_file_path[1], "r")
-            
-            # Format medium code reference
-            medium_code_reference = "\n".join(f"Line: {line},\n Explanation: {explanation}\n" for line, explanation in medium_code_reference)
-            
-        
-        
-        
-        
-        
-        # Format prompt
-            prompt = f"""\
-The following code is a Spark2014/ADA project.\n\n
-{benchmark_file.read()}
-
-
-Here are the current mediums being thrown by gnatprove, in the form of the line referenced as the cause and an explaination:\n\n
-{medium_code_reference}
-
-Return the fixed code for the first implementation file, delimiting with 
-```ada
-
-code here
-
-```
-"""
-
-        
-    # Invoke the LLM to generate the response
-            print(benchmark_file_path[1])   
-            llm_responses = self.invoke_llm(prompt)
-            
-            # Used to index the differnt solutions in the log
-            response_number_counter = 0
-            
-            for llm_response in llm_responses:
-                
-                response_number_counter += 1
-                
-                
-        # Extract the fixed code and write to file in the benchmark_dir
-                # Init compile success flag
-                compile_success = True
-
-                llm_code = ""
-                adb_file_path = ""
-
-                # Extract the code from the response
-                try:
-                    llm_code = extract_code_from_response(llm_response)
-                except ValueError as e:
-                    compile_success = False
-                    self.logger.error(
-                        f"\n-----------------------------------\nError extracting code from response number: {gpr_file_path.split('/')[-1]} : attempt {response_number_counter}\nCode: {llm_response}\n-----------------------------------\n\n")
-
-                if llm_code not in ["", None]:
-
-                    # Extract the filename from the response
-                    try:
-                        adb_filename = extract_filename_from_response(
-                            llm_code)
-
-                        # Convert filename to lowercase and add .adb extension
-                        filename_with_extension = adb_filename.lower() + ".adb"
-
-                        project_dir = "/".join(gpr_file_path.split("/")[:-1])
-                        adb_file_path = project_dir + "/" + filename_with_extension
-
-                        # Overwrite the destination file with the response code
-                        overwrite_destination_file_with_string(
-                            adb_file_path, llm_code)
-
-                    except ValueError as e:
-                        compile_success = False
-                        self.logger.error(
-                            f"\n-----------------------------------\n\nError extracting filename from response: {gpr_file_path.split('/')[-1]} : attempt {response_number_counter}\nCode: {llm_code}\n-----------------------------------\n\n")
-
-
-            
-            
-            # Three cases:
-            # 1. Code was successfully extracted and gnatprove made it to stage 2. of compilation
-            # 2. Code was successfully extracted but gnatprove did not make it to stage 2. of compilation
-            # 3. Code was not successfully extracted and gnatprove did not run
-                
-                
-                if compile_success == True:
-
-                    # Run gnatprove on the project
-                    gnatprove_output = run_gnatprove(gpr_file_path)
-                    gnatprove_successful_compilation = is_compilation_successful(
-                        gnatprove_output)
-
-                    # Case 1
-                    # Code was successfully extracted and gnatprove made it to stage 2. of compilation
-                    if gnatprove_successful_compilation:
-
-                        # Parse the new mediums
-                        new_mediums = parse_gnatprove_output(gnatprove_output)
-
-                        self.results.append((gpr_file_path.split(
-                            "/")[-1], compile_success, len(new_mediums) == 0))
-                        
-                        
-                        # Logging
-                        self.logger.info(
-                            f"Project: {gpr_file_path.split('/')[-1]} : attempt {response_number_counter} \n\nResponse: \n{nl.join(compute_diff(retrieve_package_body(benchmark_file_path[1]), llm_code))}\n\nNew Mediums: \n{new_mediums}\n\nGnatprove Output: \n{gnatprove_output} \n-----------------------------------\n\n")
-
-                    # Case 2
-                    # Code was successfully extracted but gnatprove did not make it to stage 2. of compilation
-                    else:
-                        self.results.append(
-                            (gpr_file_path.split("/")[-1], compile_success, False))
-
-                        # Logging
-                        self.logger.info(
-                            f"Project: {gpr_file_path.split('/')[-1]} : attempt {response_number_counter} \n\nResponse: \n{nl.join(compute_diff(retrieve_package_body(benchmark_file_path[1]), llm_code))}\n\nGnatprove Output: \n{gnatprove_output} \n-----------------------------------\n\n")
-
-                # Case 3
-                #  Code was not successfully extracted and gnatprove did not run
-                else:
-
-                    self.results.append(
-                        (gpr_file_path.split("/")[-1], False, False))
-
-                    # Logging
-                    self.logger.info(
-                        f"Project: {gpr_file_path.split('/')[-1]} : attempt {response_number_counter}\nError: GnatProve did not run. Either the filename or code could not be extracted from the response.\n\nResponse: \n{llm_code}\n-----------------------------------\n\n")
-
-        # End the run and log summary
-        self.end_run(self.results)
-
-
-
-
-
-
-
-    
-    
-    
-    
-    def chain_of_thoughts(self) -> None:
-        """
-        This class runs the benchmark, using the chain of thoughts prompting technique
+        This class runs the benchmark
         """
         
         
         # Initialise the run and retrieve the benchmark files
-        benchmark_files = self.init_run("Zero Shot Chain of Thoughts")
+        benchmark_files = self.init_run()
         
         
     # Iterate over each project in the benchmark
@@ -450,61 +304,44 @@ code here
             # f string newline fix   
             nl = "\n"
             
-            dependencies = retrieve_dependencies(benchmark_file_path[1])
+            # Get dependencies and format as a string, get package_body
+            dependencies = nl.join(retrieve_dependencies(benchmark_file_path[1]))
             package_body = retrieve_package_body(benchmark_file_path[1])
             
-            
-            zero_shot_CoT_prompt = f"""\n
-Try to solve the following problem logically and step by step. The final answer should then be delimited in the following way:
-
-```ada
-
-code here
-
-```
-
-The following are the specifications and dependencies of a Spark2014/ADA project:
-
-{nl.join(dependencies)} \n
-
-This is the package body (implementation):
-
-{package_body} \n
-
-Add a single 'pragma Loop_Invariant' statement to the package body, so that the code runs error and medium free. 
-Do not modify the code in any other way. Return the entire implementation file with the single addition.
-
-                    """
+            # Format the prompt
+            prompt = self.prompt.format(dependencies=dependencies, package_body=package_body)
+            print(f"{prompt}\n\n")
                     
             
     # Invoke the LLM to generate the response
             print(benchmark_file_path[1])   
-            llm_responses = self.invoke_llm(zero_shot_CoT_prompt)
+            llm_responses = self.invoke_llm(prompt)
             
             # Used to index the differnt solutions in the log
             response_number_counter = 0
             
+            
+    # For each response, extract the fixed code and write to file in the benchmark_dir
             for llm_response in llm_responses:
                 
                 response_number_counter += 1
 
-        # Extract the fixed code and write to file in the benchmark_dir
-
-                # Init compile success flag
+                # Init variables
                 compile_success = True
-
                 llm_code = ""
                 adb_file_path = ""
 
                 # Extract the code from the response
                 try:
                     llm_code = extract_code_from_response(llm_response)
+                
+                # If the code cannot be extracted, set the compile success flag to False and log the error
                 except ValueError as e:
                     compile_success = False
                     self.logger.error(
                         f"\n-----------------------------------\nError extracting code from response number: {gpr_file_path.split('/')[-1]} : attempt {response_number_counter}\nCode: {llm_response}\n-----------------------------------\n\n")
 
-                    
+                # If code is not empty, extract the filename 
                 if llm_code not in ["", None]:
 
                     # Extract the filename from the response
@@ -523,6 +360,7 @@ Do not modify the code in any other way. Return the entire implementation file w
                         overwrite_destination_file_with_string(
                             adb_file_path, llm_code)
 
+                    # If the filename cannot be extracted, set the compile success flag to False and log the error
                     except ValueError as e:
                         compile_success = False
                         self.logger.error(
@@ -580,7 +418,3 @@ Do not modify the code in any other way. Return the entire implementation file w
 
         # End the run and log summary
         self.end_run(self.results)
-
-                    
-
-            
